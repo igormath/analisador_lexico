eb4c318b46465809405a18e541e3c529bf38a458
igormath <igorxt9@gmail.com>
2025-04-09 23:06:33 -0300

    update main and token_id w/ new operators and keywords

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 code_string = ''
-palavraAtual = ''
 tokenList = []
 linhaAtual = 1
 from token_id import identify_token
@@ -14,14 +13,130 @@ i = 0
 while i < len(code_string):
     if (code_string[i] == '(' or code_string[i] == ')' or code_string[i] == '{' or code_string[i] == '}' or code_string[i] == ';'):
         identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '+'):
+        identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '-'):
+        identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '*'):
+        identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '/'):
+        identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '%'):
+        identify_token(char=code_string[i], linha=linhaAtual, tokenList=tokenList)
+
+    elif (code_string[i] == '!'):
+        if (code_string[i + 1] == '='):
+            identify_token(char='!=', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+        else:
+            identify_token(char='!', linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '&'):
+        if (code_string[i + 1] == '&'):
+            identify_token(char='&&', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+
+    elif (code_string[i] == '|'):
+        if (code_string[i + 1] == '|'):
+            identify_token(char='||', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+
+    elif (code_string[i] == '='):
+        if (code_string[i + 1] == '='):
+            identify_token(char='==', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+        else:
+            identify_token(char='=', linha=linhaAtual, tokenList=tokenList)
+
+    
+    elif (code_string[i] == '>'):
+        if (code_string[i + 1] == '='):
+            identify_token(char='>=', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+        else:
+            identify_token(char='>', linha=linhaAtual, tokenList=tokenList)
+    
+    elif (code_string[i] == '<'):
+        if (code_string[i + 1] == '='):
+            identify_token(char='<=', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+        else:
+            identify_token(char='<', linha=linhaAtual, tokenList=tokenList)
+    
     elif (code_string[i] == 'i'):
-        if (code_string[i+1] == 'n'):
-            if (code_string[i+2] == 't'):
+        if (code_string[i + 1] == 'n'):
+            if (code_string[i + 2] == 't'):
                 identify_token(char='int', linha=linhaAtual, tokenList=tokenList)
                 i += 2
+        elif (code_string[i + 1] == 'f'):
+            identify_token(char='if', linha=linhaAtual, tokenList=tokenList)
+            i += 1
+
+    elif (code_string[i] == 'm'):
+        if (code_string[i + 1] == 'a'):
+            if (code_string[i + 2] == 'i'):
+                if (code_string[i + 3] == 'n'):
+                    identify_token(char='main', linha=linhaAtual, tokenList=tokenList)
+                    i += 3
+    
+    elif (code_string[i] == 'b'):
+        if (code_string[i + 1] == 'o'):
+            if (code_string[i + 2] == 'o'):
+                if (code_string[i + 3] == 'l'):
+                    identify_token(char='bool', linha=linhaAtual, tokenList=tokenList)
+                    i += 3
+
+    elif (code_string[i] == 'f'):
+        if (code_string[i + 1] == 'l'):
+            if (code_string[i + 2] == 'o'):
+                if (code_string[i + 3] == 'a'):
+                    if (code_string[i + 4] == 't'):
+                        identify_token(char='float', linha=linhaAtual, tokenList=tokenList)
+                        i += 4
+        elif (code_string[i + 1] == 'a'):
+            if (code_string[i + 2] == 'l'):
+                if (code_string[i + 3] == 's'):
+                    if (code_string[i + 4] == 'e'):
+                        identify_token(char='false', linha=linhaAtual, tokenList=tokenList)
+                        i += 4
+
+    elif (code_string[i] == 'c'):
+        if (code_string[i + 1] == 'h'):
+            if (code_string[i + 2] == 'a'):
+                if (code_string[i + 3] == 'r'):
+                    identify_token(char='char', linha=linhaAtual, tokenList=tokenList)
+                    i += 3
+
+    elif (code_string[i] == 'e'):
+        if (code_string[i + 1] == 'l'):
+            if (code_string[i + 2] == 's'):
+                if (code_string[i + 3] == 'e'):
+                    identify_token(char='else', linha=linhaAtual, tokenList=tokenList)
+                    i += 3
+
+    elif (code_string[i] == 'w'):
+        if (code_string[i + 1] == 'h'):
+            if (code_string[i + 2] == 'i'):
+                if (code_string[i + 3] == 'l'):
+                    if (code_string[i + 4] == 'e'):
+                        identify_token(char='while', linha=linhaAtual, tokenList=tokenList)
+                        i += 4
+
+    elif (code_string[i] == 't'):
+        if (code_string[i + 1] == 'r'):
+            if (code_string[i + 2] == 'u'):
+                if (code_string[i + 3] == 'e'):
+                    identify_token(char='true', linha=linhaAtual, tokenList=tokenList)
+                    i += 3
+        
     elif (code_string[i] == '\n'):
         linhaAtual += 1
+
     i += 1
 
 print(tokenList)
- 
--- token_id.py
+++ token_id.py
@@ -1,19 +1,120 @@
 def identify_token(char, linha, tokenList):
     if (char == '('):
-        token = ('LEFTPAR', linha)
+        token = ('LEFT_PAR', linha)
         tokenList.append(token)
+
     elif (char == ')'):
-        token = ('RIGHTPAR', linha)
+        token = ('RIGHT_PAR', linha)
         tokenList.append(token)
+
     elif (char == '{'):
-        token = ('LEFTCHAV', linha)
+        token = ('LEFT_CHAV', linha)
         tokenList.append(token)
+
     elif (char == '}'):
-        token = ('RIGHTCHAV', linha)
+        token = ('RIGHT_CHAV', linha)
         tokenList.append(token)
+
     elif (char == ';'):
-        token = ('PONTOVIRGULA', linha)
+        token = ('PONTO_VIRGULA', linha)
         tokenList.append(token)
+
     elif (char == 'int'):
         token = ('KEYWORD_INT', linha)
+        tokenList.append(token)
+
+    elif (char == 'main'):
+        token = ('KEYWORD_MAIN', linha)
+        tokenList.append(token)
+
+    elif (char == 'bool'):
+        token = ('KEYWORD_BOOL', linha)
+        tokenList.append(token)
+
+    elif (char == 'float'):
+        token = ('KEYWORD_FLOAT', linha)
+        tokenList.append(token)
+    
+    elif (char == 'char'):
+        token = ('KEYWORD_CHAR', linha)
+        tokenList.append(token)
+    
+    elif (char == 'if'):
+        token = ('KEYWORD_IF', linha)
+        tokenList.append(token)
+
+    elif (char == 'else'):
+        token = ('KEYWORD_ELSE', linha)
+        tokenList.append(token)
+
+    elif (char == 'while'):
+        token = ('KEYWORD_WHILE', linha)
+        tokenList.append(token)
+    
+    elif (char == 'true'):
+        token = ('KEYWORD_TRUE', linha)
+        tokenList.append(token)
+
+    elif (char == 'false'):
+        token = ('KEYWORD_FALSE', linha)
+        tokenList.append(token)
+    
+    elif (char == '+'):
+        token = ('OPERATOR_SOMA', linha)
+        tokenList.append(token)
+
+    elif (char == '-'):
+        token = ('OPERATOR_SUBT', linha)
+        tokenList.append(token)
+
+    elif (char == '!='):
+        token = ('OPERATOR_DIFF', linha)
+        tokenList.append(token)
+
+    elif (char == '>='):
+        token = ('OPERATOR_MAIOR_OU_IGUAL_Q', linha)
+        tokenList.append(token)
+
+    elif (char == '>'):
+        token = ('OPERATOR_MAIOR_Q', linha)
+        tokenList.append(token)
+
+    elif (char == '<='):
+        token = ('OPERATOR_MENOR_OU_IGUAL_Q', linha)
+        tokenList.append(token)
+
+    elif (char == '<'):
+        token = ('OPERATOR_MENOR_Q', linha)
+        tokenList.append(token)
+
+    elif (char == '*'):
+        token = ('OPERATOR_MULT', linha)
+        tokenList.append(token)
+    
+    elif (char == '/'):
+        token = ('OPERATOR_DIV', linha)
+        tokenList.append(token)
+
+    elif (char == '%'):
+        token = ('OPERATOR_MODULO', linha)
+        tokenList.append(token)
+
+    elif (char == '='):
+        token = ('OPERATOR_ATRIBUI', linha)
+        tokenList.append(token)
+    
+    elif (char == '=='):
+        token = ('OPERATOR_IGUAL', linha)
+        tokenList.append(token)
+
+    elif (char == '!'):
+        token = ('OPERATOR_NEGACAO', linha)
+        tokenList.append(token)
+
+    elif (char == '&&'):
+        token = ('OPERATOR_E_LOGICO', linha)
+        tokenList.append(token)
+
+    elif (char == '||'):
+        token = ('OPERATOR_OU_LOGICO', linha)
         tokenList.append(token)
